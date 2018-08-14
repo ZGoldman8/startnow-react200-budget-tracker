e22d70609b9a9277c02ec87ce58b68b0ea231911
Your Name <ZGoldman@protonmail.com>
2018-08-13 19:38:18 -0700

    third commit

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './app';
-
-import React from 'react';
-import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './app';
 import store from './rootStore';
