f4e7005d97c6a0efcd2c56569e7c5a3292d33f29
Your Name <ZGoldman@protonmail.com>
2018-08-15 18:57:24 -0700

    fourth commit

--- src/js/components/IncomeEntries/IncomeEntries.jsx
+++ src/js/components/IncomeEntries/IncomeEntries.jsx
@@ -1,29 +1,55 @@
 import React from 'react';
 
-export default class Summary extends React.Component {
+export default class IncomeEntries extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className='card border-info mb-3'>
-        <div className='card-header text-white bg-info'>Summary</div>
+      <div className='card border-danger mb-3'>
+        <div className='card-header text-white bg-danger'>Income Entries</div>
         <div className='card-body'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-6 text-center'>
-                <h6 className='h6 strong'>Total Income</h6>
-                <p>$4,000.00</p>
-              </div>
-              <div className='col-6 text-center'>
-                <h6 className='h6 strong'>Total Expense</h6>
-                <p>$1,500.00</p>
+          <form>
+            <div className='form-group'>
+              <label htmlFor='income-description'>Description</label>
+              <input
+                type='text'
+                className='form-control'
+                id='income-description'
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='income-amount'>Amount</label>
+              <div className='input-group'>
+                <span className='input-group-addon'>$</span>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='income-amount'
+                />
               </div>
             </div>
-            <div className='row justify-content-center'>
-              <div className='col-6 text-center'>
-                <h6 className='h6 strong'>Left after spending</h6>
-                <p>$1,500.00</p>
-              </div>
-            </div>
-          </div>
+            <button
+              type='button'
+              className='btn btn-danger col-12 mb-5'
+            >+ Add Expense
+            </button>
+            <table className='table table-sm table-hover'>
+              <thead>
+                <tr>
+                  <th>Description</th>
+                  <th style={ { width: 120 } } >Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Rent</td>
+                  <td>$1,500.00</td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
         </div>
       </div>
     );
